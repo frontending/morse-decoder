a586c7b164f64b7a5ed4d07743b5fcec45d2e2c9
frontending <mymage259@gmail.com>
2022-01-29 23:15:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    let result = ''
+    for (let i = 0; i < expr.length; i = i + 10) {
+        if (expr[i] === '*') {
+            arr.push(' ')
+            continue
+        }
+        arr.push(strCorrect(strTrim(expr.slice(i, i + 10))))
+    }
+    for (let el of arr) {
+        if (el === ' ') {
+            result += ' '
+        }
+        else {
+            result += MORSE_TABLE[el]
+        }
+    }
+    return result
+}
+
+function strTrim(str) {
+    for (let i = 0; i < 10; i++) {
+        if (str[i] !== '0') {
+            return str.slice(i)
+        }
+    }
+}
+
+function strCorrect(str) {
+    let temp = ''
+    for (let i = 0; i < str.length; i = i + 2) {
+        str[i + 1] === '0' ? temp += '.' : temp += '-'
+    }
+    return temp
 }
 
 module.exports = {
